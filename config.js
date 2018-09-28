6ae42a363cb5d5101a0e10878372b971a850d148
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-09-28 13:55:05 -0300

    Add documentation (#43)
    * Add documentation
    
    * Review documentation
    
    [skip ci]

--- config.js
+++ config.js
@@ -5,10 +5,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SpritesmithPlugin = require('webpack-spritesmith');
 
 
+/**
+ * Create a initial configuration for webpack
+ *
+ * @function makeConfig
+ * @param  {string} name           The package name
+ * @param  {string} shortName      The package short name
+ * @param  {string} path           The path where the static files are generated
+ * @param  {string} publicPath     The prefix that should be added in the beggining of the emmited files
+ * @param  {string[]} extraEntries List of extra items to be inserted before default entries
+ * @param  {Plugin[]} extraPlugins List of extra plugins to be inserted after default plugins
+ * @return {Object}                Webpack configuration object
+ */
 let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPlugins=[]) => {
+  // Get git hash to add in the end of files
   // https://github.com/alleyinteractive/webpack-git-hash/issues/10
   const gitCmd = 'git rev-list -1 HEAD -- `pwd`'
   const gitHash = childProcess.execSync(gitCmd).toString().substring(0, 7);
+
+  // Remove old static resources
   childProcess.execSync(`rm -f ${path}/${shortName}-*`);
 
   let options = {
@@ -27,10 +42,12 @@ let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPl
     },
     module: {
       rules: [{
+        // Handle JS files
         test: /\.js$/,
         exclude: /(\/node_modules\/|test\.js$|\.spec\.js$)/,
         use: 'babel-loader',
       }, {
+        // Handle SCSS files
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
@@ -41,6 +58,7 @@ let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPl
           ]
         }),
       }, {
+        // Handle image optimization
         test: /.*\.(gif|png|jpe?g)$/i,
         use: [
           {
@@ -70,16 +88,19 @@ let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPl
           }
         ]
       }, {
+        // Handle SVG files inline in CSS
         test: /\.svg/,
         exclude: /node_modules/,
         use: 'svg-url-loader',
       }]
     },
     plugins: [
+      // Default CSS generation configuration
       new ExtractTextPlugin({
         filename: `${shortName}-${gitHash}.css`,
         allChunks: true
       }),
+      // Default Spritesmith configuration
       new SpritesmithPlugin({
         src: {
           cwd: 'app/sprite',
@@ -95,12 +116,15 @@ let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPl
       }),
     ]
   };
+  // Add extra entries
   for (let entry of extraEntries) {
     options.entry.unshift(entry);
   }
+  // Create source maps if in debug mode
   if (process.env.NODE_ENV === 'debug') {
     options.devtool = 'source-map';
   }
+  // Create index file (used for theme creation)
   if (fs.existsSync('app/index.html')) {
     options.plugins.push(
       new HtmlWebpackPlugin({
@@ -109,6 +133,7 @@ let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPl
       })
     );
   }
+  // Create resources file (used for addon creation)
   if (fs.existsSync('app/resources.pt')) {
     options.plugins.push(
       new HtmlWebpackPlugin({
@@ -118,6 +143,7 @@ let makeConfig = (name, shortName, path, publicPath='', extraEntries=[], extraPl
       })
     );
   }
+  // Add extra plugins
   for (let plugin of extraPlugins) {
     options.plugins.push(plugin);
   }
